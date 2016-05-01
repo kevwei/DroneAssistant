e7cd3a79dedd7ff32c3b0c4e73b3f4255b563c32
Darin Minamoto <darinm223@gmail.com>
2016-04-30 17:37:45 -0700

    Set video splash page to sample airplane video

--- DroneAssistant/ViewController.swift
+++ DroneAssistant/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: VideoSplashViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("Bundle path: ", NSBundle.mainBundle().bundlePath)
         
+        let bundlePath = NSBundle.mainBundle().pathForResource("Resources", ofType: "bundle")
+        let bundle = NSBundle(path: bundlePath!)
+        let resource = bundle?.pathForResource("test", ofType: "mp4")
+        
+        let url = NSURL.fileURLWithPath(resource!)
+        self.videoFrame = view.frame
+        self.fillMode = .ResizeAspectFill
+        self.alwaysRepeat = true
+        self.sound = true
+        self.startTime = 12.0
+        self.duration = 4.0
+        self.alpha = 0.7
+        self.backgroundColor = UIColor.blackColor()
+        self.contentURL = url
+        self.restartForeground = true
     }
 }
 
